00c3da6c665bf04ff9b318f1d2d4e5096d0775a1
Juan <jmantonaccio@gmail.com>
2024-04-26 15:48:23 -0300

    Primer commit grupo c

---
+++ src/Prueba.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Prueba {
+    public static void main(String[] args) {
+        List<String> lista = List.of("perro","gato","conejo");
+        System.out.println(lista);
+        // 1 forma de hacerlo
+        List<String> listaNueva = new ArrayList<>(lista);
+        listaNueva.add("pato");
+        System.out.println(listaNueva);
+        // 2da. forma
+        List<String> lista2Nueva = new ArrayList<>();
+        //lista2Nueva.addAll(lista); // 1 opcion
+        for (String s : listaNueva) { //2 opcion
+            lista2Nueva.add(s);
+        }
+
+    }
+}
